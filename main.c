b2d9de4d732d8028540a63c8f68edfd7a6616e0e
eunso0713 <aliennumber1one@sookmyung.ac.kr>
2023-11-16 16:01:27 +0900

    5

--- main.c
+++ main.c
@@ -6,24 +6,21 @@
 
 int main(void)
 {
-	int i;
-	int grade[5];
-	int average = 0;
+	char *pc;
+	int *pi;
+	double *pd;
 	
-	for(i=0; i<5; i++)
-	{
-		printf("grade[%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
+	pc = (char *)10000;
+	pi = (int*)10000;
+	pd = (double *)10000;
 	
+	printf("증가 전: pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
-	for(i=0;i<5;i++)
-	{
-		printf("grade[%i] = %i\n", i, *(grade+i));
-		average = average + *(grade+i);
-	}
-	printf("average = %i\n", average/5);
+	pc++;
+	pi++;
+	pd++;
 	
-	system("PAUSE");
+	printf("증가 후: pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
 	return 0;
 }
